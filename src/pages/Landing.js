4aeac0c2de8c774a0da24f46d43d4dd633994c9b
atakantekoglu <atakantekoglu@gmail.com>
2022-08-15 13:00:07 +0300

    struct the landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,9 +3,29 @@ import main from '../assets/images/main.svg';
 
 const Landing = () => {
     return (
-        <div>
-            <h1>Landing Page</h1>
-        </div>
+        <main>
+            <nav>
+                <figure>
+                    <img src={logo} alt="jobster logo" className='logo' />
+                </figure>
+            </nav>
+            <div className="container page">
+                <div className='info'>
+                    <h1>
+                        job <span>tracking</span> app
+                    </h1>
+                    <p>
+                        Chambray cray fingerstache tbh, sus craft beer shabby chic biodiesel chartreuse tumblr. Paleo vaporware cray letterpress leggings direct trade, small batch whatever flexitarian offal kombucha affogato next level.
+                    </p>
+                    <button className='btn btn-hero'>
+                        Login/Register
+                    </button>
+                </div>
+                <figure>
+                    <img src={main} alt="job hunt" className='img main-img' />
+                </figure>
+            </div>
+        </main>
     );
 };
 
