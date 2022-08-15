48de39457d44f7bdcbed1492025852b634c1016c
atakantekoglu <atakantekoglu@gmail.com>
2022-08-15 21:00:02 +0300

    create user slice and redux store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import 'normalize.css';
 import './index.css';
 import App from './App';
 
+// redux toolkit
+import { store } from './store';
+import { Provider } from 'react-redux';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App tab='home' />
+      </Provider>
   </React.StrictMode>
 );
